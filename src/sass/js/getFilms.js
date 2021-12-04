788c5321a3b09a7f34a96929d9a56de2b039c8bd
Dmitriy Korolov <adrorecords@gmail.com>
2021-12-04 10:49:29 +0200

    update

---
+++ src/sass/js/getFilms.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export async function getFilms(name, page) {
+    // const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${name}&safesearch=true&per_page=40&page=${page}&key=24332241-c798d1feff33a91af8e5caf46`;
+    // return await axios.get(url);
+}
