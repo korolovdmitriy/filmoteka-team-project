7582f7fc8e611bd74f1a2a12dd48a30d250917a9
ChornaEva <chornaja.evgenija@gmail.com>
2021-12-04 11:27:43 +0200

    added files from main

---
+++ src/sass/js/markup.js
@@ -0,0 +1,9 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { getRefs } from "./getRefs";
+
+const refs = getRefs();
+
+export function markup(films) {
+    
+}
