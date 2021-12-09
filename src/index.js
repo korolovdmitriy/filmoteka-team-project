fbe7013b9e53c0cb37537ad4fb70729389c25cbd
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-12-09 20:18:22 +0200

    add get from LS

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import './js/modal-film-card.js';
 import './js/modal-window-team.js';
 import './js/team-data.js';
 import './js/spiner';
-import './js/pagination.js';
+// import './js/pagination.js';
 import './js/my-library';
--- src/js/my-library.js
+++ src/js/my-library.js
@@ -1,16 +1,51 @@
+import { fetchMovies } from './apiService';
+
+const WATCHED_FILMS = 'watched-films';
+const QUEUE_FILMS = 'queue-films';
+
 const watchedBth = document.querySelector('[data-watched]');
 const queueBth = document.querySelector('[data-queue]');
 const filmsContainer = document.querySelector('.films');
 
-window.addEventListener('DOMContentLoaded', () => {
-  watchedBth.focus();
-  filmsContainer.innerHTML = '<div class="empty-list">Список просмотренных фильмов еще пуст</div>';
+watchedBth.addEventListener('click', e => {
+  e.currentTarget.classList.add('active');
+  e.currentTarget.nextElementSibling.classList.remove('active');
+  getWatchedFilmsFromLS();
 });
 
-watchedBth.addEventListener('click', () => {
-  filmsContainer.innerHTML = '<div class="empty-list">Список просмотренных фильмов еще пуст</div>';
+queueBth.addEventListener('click', e => {
+  e.currentTarget.classList.add('active');
+  e.currentTarget.previousElementSibling.classList.remove('active');
+  getQueueFilmsFromLS();
 });
 
-queueBth.addEventListener('click', () => {
-  filmsContainer.innerHTML = '<div class="empty-list">Список фильмов для просмотра еще пуст</div>';
-});
+function getWatchedFilmsFromLS() {
+  const dataFromLS = JSON.parse(localStorage.getItem(WATCHED_FILMS));
+
+  if (dataFromLS) {
+    dataFromLS.forEach(el => fetchMovies(el).then(res => console.log(res)));
+  } else {
+    filmsContainer.innerHTML = '<div class="empty-list">My watched films library is empty</div>';
+  }
+}
+
+function getQueueFilmsFromLS() {
+  const dataFromLS = JSON.parse(localStorage.getItem(QUEUE_FILMS));
+
+  if (dataFromLS) {
+    dataFromLS.forEach(el => fetchMovies(el).then(res => console.log(res)));
+  } else {
+    filmsContainer.innerHTML = '<div class="empty-list">My queue films library is empty</div>';
+  }
+}
+
+function setWatchedFilmToLS(data) {
+  localStorage.setItem(WATCHED_FILMS, JSON.stringify([...data]));
+}
+
+function setQueueFilmToLS(data) {
+  localStorage.setItem(QUEUE_FILMS, JSON.stringify([...data]));
+}
+
+// setWatchedFilmToLS(['520492', '585220']);
+// setQueueFilmToLS(['580492', '570520']);
