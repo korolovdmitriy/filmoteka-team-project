4f6526d44a9848635912bb82f79565ac551476a9
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2021-12-12 20:39:26 +0200

    Правит ошибки

--- src/js/getPopularityFilm.js
+++ src/js/getPopularityFilm.js
@@ -7,40 +7,8 @@ const refs = {
   error: document.querySelector('.text-error'),
 };
 let genre = [];
+
 refs.heroButton.addEventListener('click', onClick);
-window.addEventListener('DOMContentLoaded', getPopularFilms);
-
-function getPopularFilms(e) {
-  refs.filmsUl.innerHTML = '';
-  fetchResults(1).then(data => renderPopularFilms(data.results));
-}
-
-function renderPopularFilms(film) {
-  const markup = film
-    .map(
-      ({ release_date, original_title, genre_ids, poster_path, vote_average, overview }) =>
-        `<li class="films__item">
-      <div class="film__link">
-        <img class="film__img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" />
-        <p class="film__name">${original_title}</p>
-      </div>
-      <div class="film__box">
-        <p class="film__info">
-          <span>${genre_ids
-            .map(id => genre[id])
-            .slice(0, 2)
-            .join('')}</span>
-          &nbsp;|&nbsp;
-        </p>
-        <p class="film__info">${new Date(release_date).getFullYear()}</p>
-        <p class="rating">${vote_average}</p>
-      </div>
-      </li>
-      `,
-    )
-    .join('');
-  refs.filmsUl.insertAdjacentHTML('beforeend', markup);
-}
 
 function onClick(e) {
   const query = refs.inputHero.value;
@@ -60,7 +28,7 @@ function renderMarkup(film) {
   const markup = film
     .map(
       ({ release_date, original_title, genre_ids, poster_path, vote_average, overview }) =>
-        `<li class="films__item">
+        `<li class="films__item" >
       <div class="film__link">
         <img class="film__img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" />
         <p class="film__name">${original_title}</p>
