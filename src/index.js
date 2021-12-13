642abada384a758dc845c14940156720ab10b415
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-12-13 14:13:35 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './js/modal-window-team.js';
 // import './js/team-data.js';
 import './js/spiner';
 import './js/pagination.js';
-// import './js/firebaseGoogAuth';
 import './js/popular';
+import './js/firebaseGoogAuth';
