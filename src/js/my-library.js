71674f9697532d4c583798e30b2788d67f212257
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-12-13 23:33:12 +0200

    add library

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -1,51 +1,76 @@
 import { fetchMovies } from './apiService';
 
-const WATCHED_FILMS = 'watched-films';
-const QUEUE_FILMS = 'queue-films';
+const WATCHED_FILMS = 'watch';
+const QUEUE_FILMS = 'queue';
 
 const watchedBth = document.querySelector('[data-watched]');
 const queueBth = document.querySelector('[data-queue]');
-const filmsContainer = document.querySelector('.films');
+const filmsList = document.querySelector('.films__list');
+const emptyList = document.querySelector('.empty-list');
 
-watchedBth.addEventListener('click', e => {
-  e.currentTarget.classList.add('active');
-  e.currentTarget.nextElementSibling.classList.remove('active');
-  getWatchedFilmsFromLS();
+window.addEventListener('DOMContentLoaded', () => {
+  getFilmsFromLS(WATCHED_FILMS);
 });
 
-queueBth.addEventListener('click', e => {
-  e.currentTarget.classList.add('active');
-  e.currentTarget.previousElementSibling.classList.remove('active');
-  getQueueFilmsFromLS();
-});
+watchedBth &&
+  watchedBth.addEventListener('click', e => {
+    e.currentTarget.classList.add('active');
+    queueBth.classList.remove('active');
+    filmsList.innerHTML = '';
+    getFilmsFromLS(WATCHED_FILMS);
+  });
 
-function getWatchedFilmsFromLS() {
-  const dataFromLS = JSON.parse(localStorage.getItem(WATCHED_FILMS));
+queueBth &&
+  queueBth.addEventListener('click', e => {
+    e.currentTarget.classList.add('active');
+    watchedBth.classList.remove('active');
+    filmsList.innerHTML = '';
+    getFilmsFromLS(QUEUE_FILMS);
+  });
+
+function getFilmsFromLS(type) {
+  const dataFromLS = JSON.parse(localStorage.getItem(type));
 
   if (dataFromLS) {
-    dataFromLS.forEach(el => fetchMovies(el).then(res => console.log(res)));
+    emptyList.innerHTML = '';
+    dataFromLS.forEach(el =>
+      fetchMovies(el).then(res => {
+        filmsList.insertAdjacentHTML('afterbegin', markupFilm(res));
+      }),
+    );
   } else {
-    filmsContainer.innerHTML = '<div class="empty-list">My watched films library is empty</div>';
+    filmsList.innerHTML = '';
+    emptyList.innerHTML = `My ${type} films library is empty`;
   }
 }
 
-function getQueueFilmsFromLS() {
-  const dataFromLS = JSON.parse(localStorage.getItem(QUEUE_FILMS));
-
-  if (dataFromLS) {
-    dataFromLS.forEach(el => fetchMovies(el).then(res => console.log(res)));
-  } else {
-    filmsContainer.innerHTML = '<div class="empty-list">My queue films library is empty</div>';
-  }
+function markupFilm({ id, poster_path, title, genres, release_date, vote_average }) {
+  return `<li class="films__item" data-id="${id}">
+                <div class="film__link">
+                    <img class="film__img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${title}" />
+                </div>
+                <div class="film__box">
+                    <p class="film__name">${title}</p>
+                    <p class="film__info">
+                        <span>${genres
+                          .map(el => el.name)
+                          .slice(0, 2)
+                          .join(', ')}</span>
+                        &nbsp;|&nbsp;
+                        ${new Date(release_date).getFullYear()}
+                    </p>
+                    <p class="rating">${vote_average}</p>
+                </div>
+            </li>`;
 }
 
-function setWatchedFilmToLS(data) {
-  localStorage.setItem(WATCHED_FILMS, JSON.stringify([...data]));
-}
+// function setWatchedFilmToLS(data) {
+//   localStorage.setItem(WATCHED_FILMS, JSON.stringify([...data]));
+// }
 
-function setQueueFilmToLS(data) {
-  localStorage.setItem(QUEUE_FILMS, JSON.stringify([...data]));
-}
+// function setQueueFilmToLS(data) {
+//   localStorage.setItem(QUEUE_FILMS, JSON.stringify([...data]));
+// }
 
-// setWatchedFilmToLS(['520492', '585220']);
-// setQueueFilmToLS(['580492', '570520']);
+// setWatchedFilmToLS(['580489', '770254', '634649']);
+// setQueueFilmToLS(['585221', '770254']);
