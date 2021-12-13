36a8b6c3764b059cff0c46048ad3bfca564f7a3c
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2021-12-13 18:28:01 +0200

    Update getSearchFilm.js

--- src/js/getSearchFilm.js
+++ src/js/getSearchFilm.js
@@ -1,5 +1,14 @@
 import { fetchSearch } from './apiService';
 
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = 'cfdb2a8aab50d545dc8a1d0938de62d8';
+
+async function fetchGenres() {
+  const urlSearch = `${BASE_URL}genre/movie/list?api_key=${API_KEY}&language=en-US`;
+  const response = await fetch(urlSearch);
+  return await response.json();
+}
+
 const refs = {
   inputHero: document.querySelector('.hero__input'),
   filmsUl: document.querySelector('.films__list'),
@@ -12,14 +21,20 @@ refs.heroButton.addEventListener('click', onClick);
 function onClick(e) {
   const queryValue = refs.inputHero.value;
   if (queryValue === '') {
-    return (refs.error.textContent = 'Search result not successful. Enter the correct movie name');
+    refs.filmsUl.innerHTML = '';
+    return refs.error.classList.remove('visually-hidden');
   }
-  refs.error.classList.add('.visually-hidden');
 
-  refs.filmsUl.innerHTML = '';
   fetchSearch(1, queryValue).then(data => {
+    console.log(data.total_results);
+    if (data.total_results < 1) {
+      refs.inputHero.value = '';
+      refs.inputHero.placeholder = 'Не найдено подходящих результатов';
+    }
     renderMarkup(data.results);
   });
+  refs.error.classList.add('visually-hidden');
+  refs.filmsUl.innerHTML = '';
 }
 
 function renderMarkup(film) {
@@ -33,7 +48,7 @@ function renderMarkup(film) {
       </div>
       <div class="film__box">
         <p class="film__info">
-          <span>${genre_ids}</span>
+          <span>${fetchGenres(genre_ids)}</span>
           &nbsp;|&nbsp;
         </p>
         <p class="film__info">${new Date(release_date).getFullYear()}</p>
